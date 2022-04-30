b469890f7ac7566781924aed4d1d1d0cf9ea0b81
Itayeylath <101014251+Itayeylath@users.noreply.github.com>
2022-04-30 22:28:25 +0300

    create a checkers board 8X8 with images and array of 24 Pieces

--- app.js
+++ app.js
@@ -1,3 +1,74 @@
 //Constant variables
+const BOARD_SIZE = 8;
+const WHITE_PLAYER = 'white';
+const BLACK_PLAYER = 'black';
 
 //no-Constant variables
+let boardData;
+let pieces = [];
+
+class BoardData {
+    constructor(pieces) {
+        this.pieces = pieces;
+    }
+
+}
+
+class Piece {
+    constructor(row, col, type, player) {
+        this.row = row;
+        this.col = col;
+        this.type = type;
+        this.player = player;
+    }
+}
+
+//add image to cell
+function addImg(cell, player, name) {
+    const img = document.createElement("img")
+    img.src = 'images/' + player + '/' + name + '.png';
+    img.draggable = false;
+    cell.appendChild(img);
+}
+// Create 24 Pieces and images for new game
+function initialPieces(row, col, cell, counterPieces) {
+    if (counterPieces < 12) {
+        addImg(cell, BLACK_PLAYER, 'pawn');
+        pieces.push(new Piece(row, col, 'pawn', BLACK_PLAYER));
+    }
+    else if (counterPieces > 19) {
+        addImg(cell, WHITE_PLAYER, 'pawn');
+        pieces.push(new Piece(row, col, 'pawn', WHITE_PLAYER));
+    }
+}
+//create a checkers board 8X8 and 24 Pieces.
+function creatCheckersBoard() {
+    table = document.createElement('table');
+    document.body.appendChild(table);
+    let counterPieces = 0;
+    for (let row = 0; row < BOARD_SIZE; row++) {
+        const rowElemnt = table.insertRow();
+        for (let col = 0; col < BOARD_SIZE; col++) {
+            const cell = rowElemnt.insertCell();
+            if ((row + col) % 2 === 0) {
+                cell.className = 'light-cell';
+            } else {
+                cell.className = 'dark-cell';
+                initialPieces(row, col, cell, counterPieces);
+                counterPieces++;
+
+            } 
+        }
+    }
+}
+
+
+
+
+
+function initialgame() {
+    creatCheckersBoard();
+    boardData = new BoardData(pieces);
+}
+
+window.addEventListener('load', initialgame);
